92fd7c1dc7e3f534991a4c7ff1d45ed32030b728
PrashastVats1 <prashastvats@gmail.com>
2022-01-31 16:59:24 +0530

    first commit

---
+++ FirstConsoleApplication/FirstConsoleApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            Console.WriteLine("Enter 2 nos");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter choice of operation,1 is for addition, 2 is for subtraction, 3 is for multiplication, 4 is for division");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    c = a + b;
+                    break;
+                case 2:
+                    c = a - b;
+                    break;
+                case 3:
+                    c = a * b;
+                    break;
+                case 4:
+                    c = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ OopsConcepts/ConsoleApp1/leapyrcheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopsConcepts
+{
+    class leapyrcheck
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Input year");
+            int yr = Convert.ToInt32(Console.ReadLine());
+            if (yr % 4 == 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("Not leap year");
+            }
+        }
+    }
+}
