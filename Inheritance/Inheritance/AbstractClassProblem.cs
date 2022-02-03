244ce0641cbed5629ac4d4b8c6b1ccd4d8348f0b
PrashastVats1 <prashastvats@gmail.com>
2022-02-03 22:33:24 +0530

    comments

---
+++ Inheritance/Inheritance/AbstractClassProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class AbstractClassProblem
+    {
+        public static void Main2(string[] args)
+        {
+            Dog dog = new Dog();
+            Console.Write("What is the doggy's name?");
+            dog.SetName(Console.ReadLine());
+            Console.Write("The doggo's name is->");
+            Console.WriteLine(dog.GetName());
+            dog.Eat();
+        }
+
+        public class Dog : Animal
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("The doggo likes eating whatever the heck I want to feed him cuz he is stoopid like that!");
+            }
+        }
+
+        public abstract class Animal
+        {
+            private string Name;
+
+            public void SetName(string name)
+            {
+                Name = name;
+            }
+
+            public string GetName()
+            {
+                return Name;
+            }
+
+            public abstract void Eat();
+        }
+    }
+}
---
+++ Inheritance/Inheritance/VehicleInterface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class VehicleInterface
+    {
+        public static void Main3(string[] args)
+        {
+            Car car = new Car(0);
+            Console.Write("Enter fule value in numbers->");
+            int fuel = int.Parse(Console.ReadLine());
+            if (car.Refuel(fuel))
+            {
+                car.Drive();
+            }
+        }
+
+        public interface IVehicle
+        {
+            void Drive();
+            bool Refuel(int amount);
+        }
+
+        public class Car : IVehicle
+        {
+            public int Fuel { get; set; }
+
+            public Car(int fuel)
+            {
+                Fuel = fuel;
+            }
+
+            public void Drive()
+            {
+                if (Fuel > 0)
+                {
+                    Console.WriteLine("Za car be driving, i.e., Za Warudo!! Toki wo tomare");
+                }
+                else
+                {
+                    Console.WriteLine("No fuel so no driving plans cancelled!!");
+                }
+            }
+
+            public bool Refuel(int amount)
+            {
+                Fuel += amount;
+                return true;
+            }
+        }
+    }
+}
